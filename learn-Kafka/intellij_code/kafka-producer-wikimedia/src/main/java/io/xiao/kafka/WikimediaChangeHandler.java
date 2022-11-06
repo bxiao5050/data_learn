e3bbb64a4eb0186000819de17cf714ee3acc2223
bxiao5050 <bxiao5050@gmail.com>
2022-11-06 21:35:49 +0100

    new commit

---
+++ learn-Kafka/intellij_code/kafka-producer-wikimedia/src/main/java/io/xiao/kafka/WikimediaChangeHandler.java
@@ -0,0 +1,46 @@
+package io.xiao.kafka;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikimediaChangeHandler implements EventHandler {
+    KafkaProducer<String, String> producer;
+    String topic;
+    private final Logger log = LoggerFactory.getLogger(WikimediaChangeHandler.class.getSimpleName());
+
+
+    public WikimediaChangeHandler(KafkaProducer<String, String> producer, String topic) {
+        this.producer = producer;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() throws Exception {
+
+    }
+
+    @Override
+    public void onClosed() throws Exception {
+        this.producer.close();
+    }
+
+    @Override
+    public void onMessage(String event, MessageEvent messageEvent) throws Exception {
+        log.info(messageEvent.getData());
+        this.producer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String comment) throws Exception {
+
+    }
+
+    @Override
+    public void onError(Throwable t) {
+
+    }
+}
